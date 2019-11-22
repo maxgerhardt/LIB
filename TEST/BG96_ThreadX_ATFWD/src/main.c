d3ded4f00047635debe45ff6ea2353db536ea8d5
Georgi Angelov <the.wizarda@gmail.com>
2019-11-22 13:07:23 +0200

    Add files via upload

--- TEST/BG96_ThreadX_ATFWD/src/main.c
+++ TEST/BG96_ThreadX_ATFWD/src/main.c
@@ -71,17 +71,17 @@ int quectel_dbg_uart_init(qapi_UART_Port_Id_e port_id)
 #include "quectel_utils.h"
 #include "quectel_uart_apis.h"
 typedef unsigned char boolean;
-#define QUEC_AT_RESULT_ERROR_V01 0 /**<  Result ERROR.                                    \ \ \ \ \
-                                         This is to be set in case of ERROR or CME ERROR. \ \ \ \ \
-                                         The response buffer contains the entire details. */
-#define QUEC_AT_RESULT_OK_V01 1 /**<  Result OK. This is to be set if the final response \    \    \    \    \
-                                                     must send an OK result code to the terminal.       \ \ \ \ \
-                                                     The response buffer must not contain an OK.  */
-#define QUEC_AT_MASK_EMPTY_V01 0   /**<  Denotes a feed back mechanism for AT reg and de-reg */
-#define QUEC_AT_MASK_NA_V01 1     /**<  Denotes presence of Name field  */
-#define QUEC_AT_MASK_EQ_V01 2     /**<  Denotes presence of equal (=) operator  */
-#define QUEC_AT_MASK_QU_V01 4     /**<  Denotes presence of question mark (?)  */
-#define QUEC_AT_MASK_AR_V01 8     /**<  Denotes presence of trailing argument operator */
+#define QUEC_AT_RESULT_ERROR_V01 0  /**<  Result ERROR.
+                                          This is to be set in case of ERROR or CME ERROR.
+                                          The response buffer contains the entire details. */
+#define QUEC_AT_RESULT_OK_V01 1     /**<  Result OK. This is to be set if the final response
+                                          must send an OK result code to the terminal.
+                                          The response buffer must not contain an OK.  */
+#define QUEC_AT_MASK_EMPTY_V01 0    /**<  Denotes a feed back mechanism for AT reg and de-reg */
+#define QUEC_AT_MASK_NA_V01 1       /**<  Denotes presence of Name field  */
+#define QUEC_AT_MASK_EQ_V01 2       /**<  Denotes presence of equal (=) operator  */
+#define QUEC_AT_MASK_QU_V01 4       /**<  Denotes presence of question mark (?)  */
+#define QUEC_AT_MASK_AR_V01 8       /**<  Denotes presence of trailing argument operator */
 static int qexample_val = 0;
 
 static int qt_atoi(char *str)
