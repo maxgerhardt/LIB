566f6252308395afef8b4d066cd582367eaa16d6
Georgi Angelov <the.wizarda@gmail.com>
2020-06-19 16:40:59 +0300

    Update i2c.c

--- MTK/i2c.c
+++ MTK/i2c.c
@@ -56,3 +56,47 @@ void dcl_i2c_set_transaction_speed(void *owner, int mode, uint16_t *Fast_Mode_Sp
         }
     } //I2C_TRANSACTION_HIGH_SPEED_MODE
 }
+
+
+I2C_TIMING = 0036
+I2C_FSPEED = 100
+I2C_SAMPLE = 0
+I2C_STEP   = 54
+I2C_Config()
+
+I2C_WRITE() 4 bytes "1234"
+
+I2C_SLAVE_ADDR       = 00A6 ( 0x53 << 1)
+I2C_INT_MASK         = 0007
+I2C_INT_STAT         = 0000
+I2C_CONTROL          = 0028
+I2C_TRANSFER_LEN     = 0004
+I2C_TRANSAC_LEN      = 0001
+I2C_DELAY_LEN        = 0002
+I2C_TIMING           = 0036
+I2C_START            = 0001
+I2C_FIFO_STAT        = 0440
+I2C_FIFO_ADDR_CLR    = 0000
+I2C_IO_CONFIG        = 0000
+I2C_HS               = 0002
+I2C_TRANSFER_LEN_AUX = 0001
+I2C_TIMEOUT          = FFFF
+
+after 100 ms
+
+I2C_SLAVE_ADDR       = 00A6
+I2C_INT_MASK         = 0007
+I2C_INT_STAT         = 0000
+I2C_CONTROL          = 0028
+I2C_TRANSFER_LEN     = 0004
+I2C_TRANSAC_LEN      = 0001
+I2C_DELAY_LEN        = 0002
+I2C_TIMING           = 0036
+I2C_START            = 0001 <------
+I2C_FIFO_STAT        = 0440
+I2C_FIFO_ADDR_CLR    = 0000
+I2C_IO_CONFIG        = 0000
+I2C_HS               = 0002
+I2C_TRANSFER_LEN_AUX = 0001
+I2C_TIMEOUT          = FFFF
+--------------------------------------------
