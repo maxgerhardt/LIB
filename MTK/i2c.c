27d2ec226582f1ee6ea03574a64dbb669270dcff
Georgi Angelov <the.wizarda@gmail.com>
2020-06-22 20:58:57 +0300

    Update i2c.c

--- MTK/i2c.c
+++ MTK/i2c.c
@@ -1,47 +1,44 @@
 
-PCTL_PowerUp(PD_I2C);
-GPIO_Setup(GPIO43, GPMODE(GPIO43_MODE_SCL) | GPPULLEN | GPPUP); 
-GPIO_Setup(GPIO44, GPMODE(GPIO44_MODE_SDA) | GPPULLEN | GPPUP); 
+static uint32_t spi_transfer(spi_context *ctx, uint8_t *wr_buf, uint8_t *rd_buf, unsigned int size)
+{
+    if (spi_in_use(ctx) || 0 == size || NULL == wr_buf || NULL == rd_buf)
+        return 0;
+    if (size > SPI_MAX_PACKET_LENGTH)
+        size = SPI_MAX_PACKET_LENGTH; // 1024
+    memcpy(spi_tx_buffer, wr_buf, size);
+    SPI_TX_ADDR_REG = (int)spi_tx_buffer;
+    SPI_RX_ADDR_REG = (int)spi_rx_buffer;
+    SPI_COMM.DMA_EN_TX = SPI_MODE_DMA;
+    SPI_COMM.DMA_EN_RX = SPI_MODE_DMA;
+    SPI_CONF1.PACKET_LENGTH = size - 1; // max 1024
+    SPI_CONF1.PACKET_LOOP_CNT = 0;
 
-I2C_TIMING = 0036
-I2C_FSPEED = 100 kHz
-I2C_SAMPLE = 0
-I2C_STEP   = 54
-I2C_Config()
+    SPI_COMM.ACT = 1;
+    while (SPI_STATUS1_REG & 1) // wait finish
+    {
+    }
+    SPI_STATUS1_REG = 0; // clear finish
 
-I2C_WRITE( "1234" ) 
+    memcpy(rd_buf, spi_rx_buffer, size); // return rx
+    return size;
+}
 
-I2C_SLAVE_ADDR       = 00A6 ( 0x53 << 1)
-I2C_INT_MASK         = 0007
-I2C_INT_STAT         = 0000
-I2C_CONTROL          = 0028
-I2C_TRANSFER_LEN     = 0004
-I2C_TRANSAC_LEN      = 0001
-I2C_DELAY_LEN        = 0002
-I2C_TIMING           = 0036
-I2C_START            = 0001
-I2C_FIFO_STAT        = 0440
-I2C_FIFO_ADDR_CLR    = 0000
-I2C_IO_CONFIG        = 0000
-I2C_HS               = 0002
-I2C_TRANSFER_LEN_AUX = 0001
-I2C_TIMEOUT          = FFFF
-
-after 100 ms
-
-I2C_SLAVE_ADDR       = 00A6
-I2C_INT_MASK         = 0007
-I2C_INT_STAT         = 0000
-I2C_CONTROL          = 0028
-I2C_TRANSFER_LEN     = 0004
-I2C_TRANSAC_LEN      = 0001
-I2C_DELAY_LEN        = 0002
-I2C_TIMING           = 0036
-I2C_START            = 0001 <------
-I2C_FIFO_STAT        = 0440
-I2C_FIFO_ADDR_CLR    = 0000
-I2C_IO_CONFIG        = 0000
-I2C_HS               = 0002
-I2C_TRANSFER_LEN_AUX = 0001
-I2C_TIMEOUT          = FFFF
---------------------------------------------
+int SPI_Transfer(spi_context *ctx, uint8_t *wr_buf, uint8_t *rd_buf, uint16_t size)
+{
+    int res, cnt = 0;
+    uint8_t *wr = wr_buf;
+    uint8_t *rd = rd_buf;
+    while (size)
+    {
+        res = spi_transfer(ctx, wr, rd, size);
+        size -= res;
+        cnt += res;
+        if (0 == res)
+            return cnt;
+        if (wr_buf)
+            wr += res;
+        if (rd_buf)
+            rd += res;
+    }
+    return cnt;
+}
