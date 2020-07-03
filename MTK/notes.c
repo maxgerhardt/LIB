b8d1add573bc36e5ef526dc0ae2aeb1002aa4cda
Georgi Angelov <the.wizarda@gmail.com>
2020-07-03 14:37:17 +0300

    Update notes.c

--- MTK/notes.c
+++ MTK/notes.c
@@ -1,7 +1,7 @@
-SIM Set to GPIO mode first
-PMU_VSIM1_CON2 | 0x0002
-PMU_VSIM2_CON2 | 0x0002
-
-on/off
-PMU_VSIM1_CON0 |1 ~1
-PMU_VSIM2_CON0
+void sim1_to_gpio(unsigned volt_3)
+{
+    VSIM1_CON2 |= 2;           // 1: SIM1 is controlled by VSIM1_CON0
+    VSIM1_CON0 = 0x0400;       // reset value
+    VSIM1_CON0 |= volt_3 << 4; // 0:1.8v, 1:3.0v
+    VSIM1_CON0 |= 1;           // 1: VSIM1 Enable
+}
