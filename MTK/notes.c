77bb92f1d93971f89b0a3c0a4e6ead5bbe8feeb5
Georgi Angelov <the.wizarda@gmail.com>
2020-07-04 22:42:51 +0300

    Update notes.c

--- MTK/notes.c
+++ MTK/notes.c
@@ -5,3 +5,11 @@ void sim1_to_gpio(unsigned volt_3)
     VSIM1_CON0 |= volt_3 << 4; // 0:1.8v, 1:3.0v
     VSIM1_CON0 |= 1;           // 1: VSIM1 Enable
 }
+
+//////// DSP ////////////////////////////////////////////////////////////////
+
+#define  HW_WRITE(ptr,data)         (*(ptr) = (data))
+#define  HW_READ(ptr)               (*(ptr))
+
+#define  DSP_READ(addr)             HW_READ(addr)
+#define  DSP_WRITE(addr, data)      HW_WRITE(addr, (unsigned short)(data) )
