0015c089e602d9dbd00d1654c2c796d535bab5f0
Georgi Angelov <the.wizarda@gmail.com>
2020-07-07 21:50:55 +0300

    Update notes.c

--- MTK/notes.c
+++ MTK/notes.c
@@ -1,74 +1,23 @@
-#ifndef __DSP_H
-#define __DSP_H
+void HW_TDMA_Start(void)
+{
+   HW_WRITE(PDN_CLR2, PDN_CON_TDMA); /*power on TDMA*/
+    
+   /* Enable FRC for FM. */
+   //HW_WRITE( FRC_CON, (CMD_MAGIC_VALUE+0x00000001) );
+   //HW_WRITE( FM_CAL, 2 ); /*calibrate 2T 32K, ideal got 1586.9T 26M*/    
+}
 
-#include <stdint.h>
-#include <hal/hal_ustimer.h>
+void WAIT_TIME_QB( short time_qb )
+{
+   unsigned short  last_qb, cur_qb;
+   short  qb_diff, qb_remain;
 
-
-#define MDCONFIG_base           0x0
-#define MODEM2G_TOPSM_base      0x0
-#define MD2GCONFG_base          0x0
-#define SHAREG2_base            0x0
-
-typedef volatile unsigned short     *SRAMADDR;      /* SRAM addr is 16 bits  */
-typedef volatile unsigned short      SRAMDATA;      /* SRAM data is 16 bits  */
-typedef volatile unsigned short     *APBADDR;       /* APB addr is 32 bits   */
-typedef volatile unsigned short      APBDATA;       /* APB data is 16 bits   */
-typedef volatile unsigned long      *APBADDR32;     /* APB addr is 32 bits   */
-typedef volatile unsigned long       APBDATA32;     /* APB data is 16 bits   */
-typedef volatile unsigned short     *DPRAMADDR;     /* DPRAM addr is 16 bits */
-typedef volatile signed short       *DPRAMADDR_S;   /* DPRAM addr is 16 bits */
-typedef volatile unsigned short      DPRAMDATA;     /* DPRAM data is 16 bits */
-
-#define HW_WRITE(ptr, data) (*(ptr) = (data))
-#define HW_READ(ptr) (*(ptr))
-
-#define DSP_READ(addr) HW_READ(addr)
-#define DSP_WRITE(addr, data) HW_WRITE(addr, (unsigned short)(data))
-
-//l1_interface.h
-
-#define  MODEM_SLV_SM_WAKEUP_REQ     ((APBADDR32)(MDCONFIG_base     +0x0500)) /* modem software control TOPSM wakeup request reg */
-#define  SW_PWR_FORCE_ON_SET         ((APBADDR32)(MODEM2G_TOPSM_base+0x0A14)) /* software power force on set   */
-#define  SW_PWR_FORCE_ON_CLR         ((APBADDR32)(MODEM2G_TOPSM_base+0x0A18)) /* software power force on clear */
-#define  SW_PWR_STA                  ((APBADDR32)(MODEM2G_TOPSM_base+0x0A24)) /* Software Power Status         */
-
-#define HW_MD2G_POWER_ON()                                                            \
-    {                                                                                 \
-        HW_WRITE(MODEM_SLV_SM_WAKEUP_REQ, HW_READ(MODEM_SLV_SM_WAKEUP_REQ) | 0x1);    \
-        HW_WRITE(SW_PWR_FORCE_ON_SET, 0x1);                                           \
-        delay_u(66);                                                                  \
-        while (HW_READ(SW_PWR_STA) != 0x1)                                            \
-            ;                                                                         \
-        HW_WRITE(MODEM_SLV_SM_WAKEUP_REQ, HW_READ(MODEM_SLV_SM_WAKEUP_REQ) & (~0x1)); \
-    }
-#define HW_MD2G_POWER_OFF()                                                           \
-    {                                                                                 \
-        HW_WRITE(SW_PWR_FORCE_ON_CLR, 0x1);                                           \
-        HW_WRITE(MODEM_SLV_SM_WAKEUP_REQ, HW_READ(MODEM_SLV_SM_WAKEUP_REQ) | 0x1);    \
-        ust_busy_wait(33);                                                            \
-        HW_WRITE(MODEM_SLV_SM_WAKEUP_REQ, HW_READ(MODEM_SLV_SM_WAKEUP_REQ) & (~0x1)); \
-    }
-
-/*--------------------------------------------*/
-/* MACRO to enable MD2GSYS bus clock gating   */
-/*--------------------------------------------*/
-#define MD2GSYS_AHB_CON0 ((APBADDR)(MD2GCONFG_base + 0x0214))
-#define HW_MD2GSYS_BUS_CG_ENA()                                           \
-    {                                                                     \
-        HW_WRITE(MD2GSYS_AHB_CON0, HW_READ(MD2GSYS_AHB_CON0) | (1 << 9)); \
-    }
-
-/*--------------------------------------------*/
-/* MACRO to set reset and unlock set FD216    */
-/*--------------------------------------------*/
-#define SHARE_DSPCON ((APBADDR)(SHAREG2_base + 0x0000)) /* DSP Control Register */
-#define DSP_RESET_BEGIN 0x0006
-#define DSP_RESET_END 0x0007
-#define HW_FD216_RESET()                                                   \
-    {                                                                      \
-        DSP_WRITE(SHARE_DSPCON, DSP_RESET_BEGIN); /* reset FD216 */        \
-        DSP_WRITE(SHARE_DSPCON, DSP_RESET_END);   /* unlock reset FD216 */ \
-    }
-
-#endif
+   qb_remain = time_qb;
+   last_qb = HW_READ( TDMA_TQCNT );
+   while( qb_remain>0 )
+   {  cur_qb = HW_READ( TDMA_TQCNT );
+      qb_diff = cur_qb-last_qb;
+      qb_remain -= (qb_diff<0) ? 1 : qb_diff;
+      last_qb = cur_qb;
+   }
+}
